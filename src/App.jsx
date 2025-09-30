3dd2261b914882e1584d9a395b595d327108972d
muhpuc40 <mdhassan49.muh@gmail.com>
2025-09-30 20:10:40 +0600

    React + talwind-css +daisyui + react-router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Root from './Root';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Root />}>
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/Root.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+
+const Root = () => {
+  return (
+    <div >
+
+    </div>
+  );
+};
+
+export default Root;
