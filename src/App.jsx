7327f24f0846300a6505229a6134c7289713fc94
ebteshampuc44 <mdebteshamazam@gmail.com>
2025-09-30 21:17:01 +0600

    Update dependencies and add Navbar, Footer, and Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Root from './Root';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Root />}>
-
+        <Route index element={<Home />} />
       </Route>
     </Routes>
   );
